68f7dc054726a7c214a694d281ac642ad658fe37
unknown <aderonkeakinyemi@gmail.com>
2020-11-24 14:53:06 +0100

    completed application is here

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -8,32 +9,49 @@ void main() {
         title: Text('Dicee'),
         backgroundColor: Colors.red,
       ),
-      body: Dicee(),
+      body: DiceApp(),
     ),
   ));
 }
 
-class Dicee extends StatelessWidget {
+class DiceApp extends StatefulWidget {
+  @override
+  _DiceAppState createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  var rNumber = new Random();
+  int LimgNumber = 1;
+  int RimgNumber = 1;
+
+  void rollDice() {
+    setState(() {
+      LimgNumber = rNumber.nextInt(6) + 1;
+      RimgNumber = rNumber.nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var imgNumber = 5;
     return Center(
       child: Row(
         children: [
           Expanded(
             child: TextButton(
               onPressed: () {
+                rollDice();
                 print('Clicked on the first Object');
               },
-              child: Image.asset('images/dice$imgNumber.png'),
+              child: Image.asset('images/dice$LimgNumber.png'),
             ),
           ),
           Expanded(
             child: TextButton(
               onPressed: () {
+                rollDice();
                 print('Clicked on the second element');
               },
-              child: Image.asset('images/dice2.png'),
+              child: Image.asset('images/dice$RimgNumber.png'),
             ),
           ),
         ],
